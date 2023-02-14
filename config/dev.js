f73cf97380de573c57619a6f2d399312727bb6d1
gusta <gustav_hassler@hotmail.com>
2023-02-14 09:52:45 +0100

    Added prod-version

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID:
+        "429007832308-rgud3k451sj0sc718108a1dqepbs298p.apps.googleusercontent.com",
+    googleClientSecret: "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",
+    mongoURI:
+        "mongodb+srv://dbArvidus89:9DBPMjlU6q08Tmws@cluster0.rztmppr.mongodb.net/emailydb?retryWrites=true&w=majority",
+    cookieKey: "qweryqwertyqwertyqwerty0",
+};
--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const { connect } = require("mongoose");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
+
+connect(keys.mongoURI);
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-const PORT = process.env.PORT || 5000;
+require("./routes/auth-routes")(app);
 
-app.listen(5000);
+app.listen(process.env.PORT || 5000);
